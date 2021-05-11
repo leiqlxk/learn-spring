b5ada05a81dbaa16b416d3deaad2f861709f3fc5
leiqlxk <472990991@qq.com>
2021-05-11 22:32:52 +0800

    advisor

---
+++ aop-base-example/src/main/java/org/lql/advisor/Seller.java
@@ -0,0 +1,17 @@
+package org.lql.advisor;
+
+/**
+ * Title: Seller <br>
+ * ProjectName: spring-boot-example <br>
+ * description: TODO <br>
+ *
+ * @author: leiql <br>
+ * @version: 1.0 <br>
+ * @since: 2021/5/11 22:17 <br>
+ */
+public class Seller {
+
+    public void greetTo(String name) {
+        System.out.println("seller greet to " + name + "...");
+    }
+}
